bf8609ac392069dfc4e7bba28eb5fa452688df34
rayesyounes <rayesyounes88@gmail.com>
2024-05-28 12:38:38 +0100

    fix build

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
         event = stripe.webhooks.constructEvent(
             body,
             signature,
-            process.env.STRIPE_SECRET_WEBHOOK as string
+            process.env.STRIPE_CONNECT_WEBHOOK_SECRET as string
         );
     } catch (error: unknown) {
         return new Response("webhook error", { status: 400 });
